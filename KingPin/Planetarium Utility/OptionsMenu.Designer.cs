11d462f611744590dbdffcea82ec455d5b54323a
Benedict Tobias <btobia9509@students.pcci.edu>
2015-03-03 10:40:10 -0600

    Design layout for Options

--- KingPin/Planetarium Utility/OptionsMenu.Designer.cs
+++ KingPin/Planetarium Utility/OptionsMenu.Designer.cs
@@ -28,19 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ComputerAliasGroupBox = new System.Windows.Forms.GroupBox();
+            this.ComputerAliasPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.computerNameTextBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.ComputerAliasGroupBox.SuspendLayout();
+            this.ComputerAliasPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 10;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Controls.Add(this.ComputerAliasGroupBox, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 10;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(563, 445);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // ComputerAliasGroupBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.ComputerAliasGroupBox, 4);
+            this.ComputerAliasGroupBox.Controls.Add(this.ComputerAliasPanel);
+            this.ComputerAliasGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ComputerAliasGroupBox.Location = new System.Drawing.Point(3, 3);
+            this.ComputerAliasGroupBox.Name = "ComputerAliasGroupBox";
+            this.tableLayoutPanel1.SetRowSpan(this.ComputerAliasGroupBox, 2);
+            this.ComputerAliasGroupBox.Size = new System.Drawing.Size(218, 82);
+            this.ComputerAliasGroupBox.TabIndex = 0;
+            this.ComputerAliasGroupBox.TabStop = false;
+            this.ComputerAliasGroupBox.Text = "Computer Alias";
+            // 
+            // ComputerAliasPanel
+            // 
+            this.ComputerAliasPanel.ColumnCount = 2;
+            this.ComputerAliasPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.35849F));
+            this.ComputerAliasPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.64151F));
+            this.ComputerAliasPanel.Controls.Add(this.label1, 0, 0);
+            this.ComputerAliasPanel.Controls.Add(this.label2, 0, 1);
+            this.ComputerAliasPanel.Controls.Add(this.computerNameTextBox, 1, 0);
+            this.ComputerAliasPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ComputerAliasPanel.Location = new System.Drawing.Point(3, 16);
+            this.ComputerAliasPanel.Name = "ComputerAliasPanel";
+            this.ComputerAliasPanel.RowCount = 2;
+            this.ComputerAliasPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ComputerAliasPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ComputerAliasPanel.Size = new System.Drawing.Size(212, 63);
+            this.ComputerAliasPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.Location = new System.Drawing.Point(3, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of Renderer";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // computerNameTextBox
+            // 
+            this.computerNameTextBox.Location = new System.Drawing.Point(113, 3);
+            this.computerNameTextBox.Name = "computerNameTextBox";
+            this.computerNameTextBox.Size = new System.Drawing.Size(96, 20);
+            this.computerNameTextBox.TabIndex = 2;
             // 
             // OptionsMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(563, 445);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "OptionsMenu";
             this.Text = "Options";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ComputerAliasGroupBox.ResumeLayout(false);
+            this.ComputerAliasPanel.ResumeLayout(false);
+            this.ComputerAliasPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox ComputerAliasGroupBox;
+        private System.Windows.Forms.TableLayoutPanel ComputerAliasPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox computerNameTextBox;
+
     }
 }
