ad30f713a8280c40a9dec4a369f45cf154fb20a3
Benedict Tobias <btobia9509@students.pcci.edu>
2015-03-03 09:24:34 -0600

    Basic setup: change form name

--- KingPin/Planetarium Utility/Program.cs
+++ KingPin/Planetarium Utility/Program.cs
@@ -16,7 +16,7 @@ namespace Planetarium_Utility
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
